e3e0779847043803c0c51cb04efd8e50e6f6c25e
MShahbazh <msh23904t@gmail.com>
2025-11-14 02:44:51 +0500

    Update Source.asm

--- Source.asm
+++ Source.asm
@@ -20,7 +20,6 @@ INCLUDE Irvine32.inc
 	t13 BYTE "-----> ",0
 	main_title DWORD OFFSET t1, OFFSET t2, OFFSET t3 ,OFFSET t4, OFFSET t5, OFFSET t6, OFFSET t7, OFFSET t8 ,OFFSET t9, OFFSET t10, OFFSET t11, OFFSET t12
 	max_width BYTE ?
-	max_height BYTE ?
 	option_flag DWORD 0
 	YELLOW = 14
 	WHITE = 15
@@ -34,7 +33,6 @@ PRINTING_MENU PROC
 	call SetTextColor
 	call GetMaxXY
 	mov max_width, dl
-	mov max_height, dh
 	mov al, max_width
 	inc al
 	sub al, LENGTHOF t1
